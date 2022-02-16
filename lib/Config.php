ee4d34d4cdaf69682dc561c7bda9912cabc83688
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2022-02-16 19:52:32 +0300

    ed

--- lib/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("SERVER","localhost");
-define("DBNAME","placewis_loyality");
-define("DBHOST","mysql:host=shopify.placewise-services.com;dbname=placewis_loyality");
-define("DBUSER","placewis_loyality");
-define("DBPASS","J%wdx4PI!jY5");
-define("root_path","https://ownframework.placewise-services.com");
-?>
